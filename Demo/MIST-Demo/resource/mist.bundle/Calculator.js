ca478994ab6e9c77c9177da75dfd5075bf27a764
pangza.lgl <pangza.lgl@alibaba-inc.com>
2017-06-29 16:07:58 +0800

    用 js 写计算器 demo 的逻辑

---
+++ Demo/MIST-Demo/resource/mist.bundle/Calculator.js
@@ -0,0 +1,88 @@
+function click(params) {
+    var button = params.button;
+    var state = params.state;
+    var maxLength = 9;
+
+    if (button.type == 'clear') {
+        if (state.isC) {
+            state.num = '0';
+            if (state.second) {
+                state.num2 = state.num;
+            }
+            else {
+                state.num1 = state.num;
+            }
+            state.isC = false;
+        }
+        else {
+            state = {};
+        }
+    }
+    else if (button.type == 'sign') {
+        if (state.clear) {
+            state.num = '0';
+            state.clear = false;
+        }
+        state.num = state.num.startsWith('-') ? state.num.substring(1) : '-' + state.num;
+    }
+    else if (button.type == 'percent') {
+        state.num = state.num / 100;
+        if (state.second) {
+            state.num2 = state.num;
+        }
+        else {
+            state.num1 = state.num;
+        }
+    }
+    else if (button.type == 'equal') {
+        if (!state.op) {
+            return;
+        }
+        if (!state.num2) {
+            state.num2 = state.num1;
+        }
+        state.num = eval((state.num1 || state.num) + state.op + (state.num2));
+        state.num1 = null;
+        state.clear = true;
+        state.second = false;
+    }
+    else if (button.type == 'point') {
+        if (state.num.includes('.') || state.num.length >= maxLength) {
+            return;
+        }
+        state.num += '.';
+    }
+    else if (button.op) {
+        if (state.num1 && state.num2 && state.second) {
+            state.num1 = state.num = eval((state.num1 || state.num) + state.op + (state.num2 || state.num));
+        }
+        state.num1 = state.num;
+        state.second = true;
+        state.op = button.op;
+        state.clear = true;
+    }
+    else {
+        state.num = (state.num == '0' || state.clear) ? button.text : (state.num == '-0' ? '-' : (state.num || '')) + button.text;
+        if (state.num.replace('.', '').length > maxLength) {
+            return;
+        }
+        if (state.second) {
+            state.num2 = state.num;
+        }
+        else {
+            state.num1 = state.num;
+        }
+        state.clear = false;
+    }
+    if (state.num && state.num != '0') {
+        state.isC = true;
+    }
+
+    state.showNum = state.num ? String(state.num).replace('-', '–').replace(/\d+/, function(s) {
+        return s.replace(/\d+?(?=(\d{3})+\b)/g, function(s){  
+            return s +',';
+        });
+    }) : '0';
+    
+    setState(state);
+}

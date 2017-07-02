9778cee42ea1e189d1f7de5af4e3f6a0207be0ff
lingwan <yunpeng.wyp@alibaba-inc.com>
2017-07-03 00:53:19 +0800

    文案更新

--- Demo/MIST-Demo/resource/mist.bundle/JSDemo.js
+++ Demo/MIST-Demo/resource/mist.bundle/JSDemo.js
@@ -1,7 +1,6 @@
 function getWeather() {
 	var url = 'http://api.openweathermap.org/data/2.5/weather?q=Hangzhou&appid=e29037c971eb05e99323a39106735690&units=metric&lang=zh'
 	http.get(url, function(response, error) {
-		oclog(response.name, response.weather[0].description, response.main.temp)
 		toast.alert('Result', [response.name, response.weather[0].description, response.main.temp].join(', '))
 	})
 }

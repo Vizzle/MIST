cd84d34e72e543d80ac39842bceae5be613ceaaf
lingwan <yunpeng.wyp@alibaba-inc.com>
2017-07-03 20:10:51 +0800

    js update

---
+++ MIST/js/VZMistJSContextBuilder.h
@@ -0,0 +1,16 @@
+//
+//  VZMistJSContextBuilder.h
+//  Pods
+//
+//  Created by lingwan on 2017/7/3.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+
+@interface VZMistJSContextBuilder : NSObject
+
++ (JSContext *)newJSContext;
+
+@end

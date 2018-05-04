b39ca1a612ae07b5c5066047a26487758f6124a7
pangza.lgl <pangza.lgl@alibaba-inc.com>
2018-05-04 12:50:30 +0800

    fix

--- MIST/core/VZFNode+Template.mm
+++ MIST/core/VZFNode+Template.mm
@@ -16,7 +16,6 @@
 #import "VZDataStructure.h"
 #import <VZFlexLayout/VZFNodeSubClass.h>
 #import <VZFlexLayout/VZFScrollView.h>
-#import <VZFlexLayout/VZFPagingView.h>
 #import <objc/runtime.h>
 #import "VZMistInternal.h"
 #import <VZFlexLayout/VZFNodeInternal.h>
@@ -788,16 +787,6 @@ static const void *displayEventKey = &displayEventKey;
         }]);
     }
 
-    VZMistTemplateEvent *panEvent = [VZMistTemplateEvent eventWithName:@"on-pan" dict:tpl expressionContext:data item:item];
-    if (panEvent) {
-        specs.gestures.push_back([VZFBlockGesture gestureWithClass:[UIPanGestureRecognizer class] block:^(UIPanGestureRecognizer *sender) {
-            CGPoint translation = [sender translationInView:sender.view];
-            [panEvent addEventParamWithName:@"translation" object:[NSValue valueWithCGPoint:translation]];
-            [panEvent addEventParamWithName:@"state" object:@(sender.state)];
-            [panEvent invokeWithSender:sender];
-        }]);
-    }
-
 #define VZMistEventDef(name, prop) \
     VZMistTemplateEvent *prop##Event = [VZMistTemplateEvent eventWithName:name dict:tpl expressionContext:data item:item]; \
     if (prop##Event) { \
@@ -1021,15 +1010,7 @@ static const void *displayEventKey = &displayEventKey;
             if (!backingViewClass || ![backingViewClass conformsToProtocol:@protocol(VZFNodeBackingViewInterface)]) {
                 backingViewClass = [VZFScrollView class];
             }
-
-            VZMistTemplateEvent *scrollEvent = [VZMistTemplateEvent eventWithName:@"on-scroll" dict:tpl expressionContext:data item:item];
-            if (scrollEvent) {
-                scrollSpecs.scrolled = [VZFBlockAction action:^(UIScrollView *sender) {
-                    [scrollEvent addEventParamWithName:@"offset" object:[NSValue valueWithCGPoint:sender.contentOffset]];
-                    [scrollEvent invokeWithSender:sender];
-                }];
-            }
-
+            
             node = [VZFScrollNode newWithScrollAttributes:scrollSpecs StackAttributes:stackSpecs NodeSpecs:specs BackingViewClass:backingViewClass Children:list];
         } else if ([@"paging" isEqualToString:type]) {
             PagingNodeSpecs pagingSpecs = PagingNodeSpecs();
@@ -1037,8 +1018,7 @@ static const void *displayEventKey = &displayEventKey;
 
             VZMistTemplateEvent *switchEvent = [VZMistTemplateEvent eventWithName:@"on-switch" dict:tpl expressionContext:data item:item];
             if (switchEvent) {
-                pagingSpecs.switched = [VZFBlockAction action:^(VZFPagingView *sender) {
-                    [switchEvent addEventParamWithName:@"page" object:@(sender.currentPage)];
+                pagingSpecs.switched = [VZFBlockAction action:^(id sender) {
                     [switchEvent invokeWithSender:sender];
                 }];
             }

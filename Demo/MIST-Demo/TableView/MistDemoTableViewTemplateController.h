67123e2b31fa6cbb92bb99f24975b9f8fe3d5fee
xta0 <jayson.xu@foxmail.com>
2018-12-21 10:04:54 -0800

    Update(auto commit)

---
+++ Demo/MIST-Demo/TableView/MistDemoTableViewTemplateController.h
@@ -0,0 +1,17 @@
+//
+//  MistDemoTableViewTemplateController.h
+//  MIST-Demo
+//
+//  Created by Tao Xu on 12/21/18.
+//  Copyright © 2018 Vizlab. All rights reserved.
+//
+
+#import "VZMistTemplateController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MistDemoTableViewTemplateController : VZMistTemplateController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/MIST-Demo/TableView/MistDemoTableViewTemplateController.m
@@ -0,0 +1,13 @@
+//
+//  MistDemoTableViewTemplateController.m
+//  MIST-Demo
+//
+//  Created by Tao Xu on 12/21/18.
+//  Copyright © 2018 Vizlab. All rights reserved.
+//
+
+#import "MistDemoTableViewTemplateController.h"
+
+@implementation MistDemoTableViewTemplateController
+
+@end
